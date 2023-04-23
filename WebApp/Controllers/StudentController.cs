aa7e8bda5308684f34f90ee2e923f0291d560c77
ayushkishore1082 <ayushkishore2633@gmail.com>
2023-04-23 22:27:29 +0530

    done

---
+++ WebApp/Controllers/StudentController.cs
@@ -0,0 +1,47 @@
+﻿using Data;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.AppCode.Helper;
+
+namespace WebApp.Controllers
+{
+    [Route("/student/")]
+    public class StudentController : Controller
+    {
+        private readonly IGenericMethods _httpRequest;
+        public StudentController(IGenericMethods httpRequest)
+        {
+            _httpRequest = httpRequest;
+        }
+
+        [HttpGet(nameof(Index))]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+        [HttpPost(nameof(Save))]
+        public async Task<IActionResult> Save(Student student)
+        {
+            var res = await _httpRequest.Get<Response>($"student/save", User?.GetLoggedInUserToken(), student);
+            return View(res);
+        }
+
+        [HttpPost(nameof(Edit))]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var res = await _httpRequest.Get<StudentColumn>($"student/GetById/{id}", User?.GetLoggedInUserToken(), new { });
+            return PartialView(res);
+        }
+
+        [HttpPost(nameof(GetAll))]
+        public async Task<IActionResult> GetAll(PagedRequest request)
+        {
+            var res = await _httpRequest.Get<PagedResult<StudentColumn>>($"student/GetAll", User?.GetLoggedInUserToken(), request);
+            return PartialView(res ?? new PagedResult<StudentColumn> { Data = new List<StudentColumn>() });
+        }
+    }
+}
